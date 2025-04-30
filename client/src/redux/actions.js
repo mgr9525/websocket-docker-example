26e79a63fad0a2ee7282c034871abc1f95e43075
LinsRuis.HW1 <mgr9525@qq.com>
2025-04-30 14:14:00 +0800

    +

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,14 +3,14 @@ export function openConnection(payload) {
     let id = null
 
     if (!payload.id) {
-      id = await fetch('http://localhost:5006/id').then(res => res.json())
+      id = await fetch('http://192.252.189.39:5006/id').then(res => res.json())
       //localStorage.setItem('id', id.id)
       dispatch({
         type: 'FETCH_ID_SUCCESS',
         payload: id
       })
     }
-    const ws = new WebSocket(`ws://localhost:5006/?id=${id ? id.id : payload.id}`)
+    const ws = new WebSocket(`ws://192.252.189.39:5006/?id=${id ? id.id : payload.id}`)
     
     ws.onmessage = (e) => {
       const payload = JSON.parse(e.data)
